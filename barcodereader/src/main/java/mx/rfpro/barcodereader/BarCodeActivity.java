d0259395741ce2adb9a5033179c9299b193e2b7c
Enrique Maldonado <enriquemromo@rfpro.mx>
2019-11-29 18:44:46 -0600

    Lectura Code bar

--- barcodereader/src/main/java/mx/rfpro/barcodereader/BarCodeActivity.java
+++ barcodereader/src/main/java/mx/rfpro/barcodereader/BarCodeActivity.java
@@ -1,5 +1,6 @@
 package mx.rfpro.barcodereader;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.Log;
@@ -37,6 +38,8 @@ public class BarCodeActivity extends AppCompatActivity {
                 scanBarcode();
             }
         });
+
+        new InitTask().execute();
 
     }
 
@@ -93,6 +96,44 @@ public class BarCodeActivity extends AppCompatActivity {
 
         Vibrator vibrator = (Vibrator)this.getSystemService(this.VIBRATOR_SERVICE);
         vibrator.vibrate(100);
+    }
+
+
+    public class InitTask extends AsyncTask<String, Integer, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            // TODO Auto-generated method stub
+
+
+            boolean reuslt=false;
+            if(barcode2DWithSoft!=null) {
+                reuslt=  barcode2DWithSoft.open(BarCodeActivity.this);
+                Log.i(TAG,"open="+reuslt);
+
+            }
+            return reuslt;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+            if(result){
+//                barcode2DWithSoft.setParameter(324, 1);
+//                barcode2DWithSoft.setParameter(300, 0); // Snapshot Aiming
+//                barcode2DWithSoft.setParameter(361, 0); // Image Capture Illumination
+
+                // interleaved 2 of 5
+
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+        }
+
     }
 
 
